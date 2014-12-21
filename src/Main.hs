68cda4e57274bfffc0d1d098fcdbd8551a850ac1
Yuya Yabe <yuyabee@gmail.com>
2014-12-20 20:45:27 -0800

    avoid unnecessary \n in the excutable

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
 import Roogle
 import System.Environment(getArgs)
 
+rstrip :: String -> String
+rstrip = reverse . dropWhile (=='\n') . reverse
+
 main :: IO ()
 main = do
     args <- getArgs
     doc <- readFile $ head args
-    putStrLn (unlines $ extractTypeSignatures typeSignaturePattern1 $ lines doc)
+    putStrLn (rstrip $ unlines $ extractTypeSignatures typeSignaturePattern1 $ lines doc)
